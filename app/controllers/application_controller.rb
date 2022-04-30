5002aea1fc19e762ee3f53dd1241729a145c5ba6
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-30 11:36:00 +0000

    [Add]bookの機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  
+
+
   def after_sign_in_path_for(resorce)
-    about_path
+    books_path(resource)
   end
-  
+
   def after_sign_out_path_for(resorce)
     about_path
   end
-  
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,9 +15,25 @@ class BooksController < ApplicationController
   end
   
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update 
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @bookshow = Book.new
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path(@book)
   end
   
   
