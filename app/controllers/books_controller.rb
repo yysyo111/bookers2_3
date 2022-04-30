20fa277c0f79bc648a80d636a688f6c8c3fc144d
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-30 03:16:58 +0000

    [start]投稿機能作成tyuu

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,25 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
-
+  
+  def create
+    @book = Book.new
+  end
+  
   def edit
   end
 
   def show
   end
+  
+  
+  private
+  
+  def book_params
+    
+  end
+  
+  
 end
